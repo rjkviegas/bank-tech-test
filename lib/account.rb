ac6ec13e25c9dbeada2a771c3194e400930313e0
Ryan Viegas <rjkviegas@gmail.com>
2020-09-07 15:36:58 +0100

    Account #deposit adds a credit transaction to the account statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,18 @@
 require_relative 'statement'
+require_relative 'transaction'
 
 class Account
-  attr_reader :balance, :statement
+  attr_reader :balance, :statement, :transaction_class
 
-  def initialize(statement_class)
+  def initialize(statement_class, transaction_class)
     @balance = 0
     @statement = statement_class.new
+    @transaction_class = transaction_class
   end
 
-  def deposit(amount)
+  def deposit(date, amount)
     @balance += amount
+    credit_transaction = @transaction_class.new(date, 'credit', amount, "#{balance}")
+    @statement.transactions.push(credit_transaction.string)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,21 @@
 require '../lib/account.rb'
 
 describe Account do
-  describe Account.new(Statement) do
+  describe Account.new(Statement, Transaction) do
     it { is_expected.to have_attributes(:balance => 0) }
     it { is_expected.to have_attributes(:statement => an_instance_of(Statement)) }
+    it { is_expected.to have_attributes(:transaction_class => Transaction) }
   end
 
   describe '#deposit' do
-    let(:statement_double) { double('statement_double', new: nil) }
-    let(:account) { Account.new(statement_double)}
+    let(:account) { Account.new(Statement, Transaction) }
     it 'adds amount to balance' do
-      account.deposit(500)
+      account.deposit('20/01/2020', 500)
       expect(account.balance).to eq(500)
+    end
+    it 'adds a credit transaction to the account statement' do 
+      account.deposit('20/01/2020', 500)
+      expect(account.statement.transactions).to eq(['date || credit || debit || balance', '20/01/2020 || 500.00 || || 500.00'])
     end
   end
 end
