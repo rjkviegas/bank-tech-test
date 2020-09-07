94111558622957a0a2692cec552d7410df9c0e58
Ryan Viegas <rjkviegas@gmail.com>
2020-09-07 12:18:04 +0100

    Statement #add string to transactions array

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,7 +1,11 @@
 class Statement
   attr_reader :transactions
-  
+
   def initialize
     @transactions = ['date || credit || debit || balance']
+  end
+
+  def add(transaction)
+    transactions << transaction
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,8 +3,17 @@ require "../lib/statement.rb"
 describe Statement do
   describe '#initialize' do
     it '#transactions' do
-      statement = Statement.new()
+      statement = Statement.new
       expect(statement).to have_attributes(:transactions => ['date || credit || debit || balance']) 
+    end
+  end
+  describe '#add' do
+    it 'string to transactions array' do
+      statement = Statement.new
+      expect(statement.transactions.length).to eq(1)
+      statement.add('transaction')
+      expect(statement.transactions.length).to eq(2)
+      expect(statement.transactions[1]).to eq('transaction')
     end
   end
 end
