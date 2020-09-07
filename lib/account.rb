bda97026bfbfe3ac0bddd8b3e8c78283581cec95
Ryan Viegas <rjkviegas@gmail.com>
2020-09-07 13:48:01 +0100

    Account #deposit adds amount to balance

--- lib/account.rb
+++ lib/account.rb
@@ -7,4 +7,8 @@ class Account
     @balance = 0
     @statement = statement_class.new
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,12 @@ describe Account do
     it { is_expected.to have_attributes(:balance => 0) }
     it { is_expected.to have_attributes(:statement => an_instance_of(Statement)) }
   end
+
+  describe '#deposit' do
+    it 'adds amount to balance' do
+      account = Account.new(Statement)
+      account.deposit(500)
+      expect(account.balance).to eq(500)
+    end
+  end
 end
