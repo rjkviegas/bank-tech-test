9c6a35135ad3c9bb8dd668d4da8164c519bfd922
Ryan Viegas <rjkviegas@gmail.com>
2020-09-09 14:56:33 +0100

    Feature Test Statement #print_to_console prints header only when no transactions exist

--- spec/feature/feature_tests_spec.rb
+++ spec/feature/feature_tests_spec.rb
@@ -3,42 +3,63 @@
 require './lib/account.rb'
 
 describe 'Feature Tests' do
+
   describe Account.new(Statement, Transaction) do
     it { is_expected.to have_attributes(balance: 0) }
     it { is_expected.to have_attributes(statement: an_instance_of(Statement)) }
     it { is_expected.to have_attributes(transaction_class: Transaction) }
   end
 
-  describe '#deposit' do
-    let(:time_double) { double('time_double', strftime: '20/01/2020') }
-    let(:account) { Account.new(Statement, Transaction) }
-    it 'adds amount to balance' do
-      expect { account.deposit(500) }.to change { account.balance }.by 500
+  describe 'Account' do
+    
+    describe '#deposit' do
+      let(:time_double) { double('time_double', strftime: '20/01/2020') }
+      let(:account) { Account.new(Statement, Transaction) }
+
+      it 'adds amount to balance' do
+        expect { account.deposit(500) }.to change { account.balance }.by 500
+      end
+
+      it 'adds a credit transaction to the account statement' do
+        allow(Time).to receive(:now) { time_double }
+        expect { account.deposit(500) }.to change { account.statement.transactions.length }.by 1
+        expect(account.statement.transactions).to include(a_kind_of(Transaction))
+        expect(account.statement.transactions[0].type).to eq('credit')
+        expect(account.statement.transactions[0].date).to eq(time_double.strftime)
+        expect(account.statement.transactions[0].amount).to eq(500)
+      end
     end
-    it 'adds a credit transaction to the account statement' do
-      allow(Time).to receive(:now) { time_double }
-      expect { account.deposit(500) }.to change { account.statement.transactions.length }.by 1
-      expect(account.statement.transactions).to include(a_kind_of(Transaction))
-      expect(account.statement.transactions[0].type).to eq('credit')
-      expect(account.statement.transactions[0].date).to eq(time_double.strftime)
-      expect(account.statement.transactions[0].amount).to eq(500)
+
+    describe '#withdrawal' do
+      let(:time_double) { double('time_double', strftime: '20/01/2020') }
+      let(:account) { Account.new(Statement, Transaction) }
+
+      it 'subtracts amount from balance' do
+        expect { account.withdrawal(250) }.to change { account.balance }.by(-250)
+      end
+
+      it 'adds a debit transaction to the account statement' do
+        allow(Time).to receive(:now) { time_double }
+        account.withdrawal(300)
+        expect { account.deposit(300) }.to change { account.statement.transactions.length }.by 1
+        expect(account.statement.transactions).to include(a_kind_of(Transaction))
+        expect(account.statement.transactions[0].type).to eq('debit')
+        expect(account.statement.transactions[0].date).to eq(time_double.strftime)
+        expect(account.statement.transactions[0].amount).to eq(300)
+      end
     end
   end
 
-  describe '#withdrawal' do
-    let(:time_double) { double('time_double', strftime: '20/01/2020') }
-    let(:account) { Account.new(Statement, Transaction) }
-    it 'subtracts amount from balance' do
-      expect { account.withdrawal(250) }.to change { account.balance }.by(-250)
-    end
-    it 'adds a debit transaction to the account statement' do
-      allow(Time).to receive(:now) { time_double }
-      account.withdrawal(300)
-      expect { account.deposit(300) }.to change { account.statement.transactions.length }.by 1
-      expect(account.statement.transactions).to include(a_kind_of(Transaction))
-      expect(account.statement.transactions[0].type).to eq('debit')
-      expect(account.statement.transactions[0].date).to eq(time_double.strftime)
-      expect(account.statement.transactions[0].amount).to eq(300)
+  describe 'Statement' do
+    
+    describe '#print_to_console' do
+
+      it 'prints header only when no transactions exist' do
+        account = Account.new(Statement, Transaction)
+        expect { account.statement.print_to_console }.to output(
+          "date || credit || debit || balance\n"
+        ).to_stdout
+      end
     end
   end
 end
