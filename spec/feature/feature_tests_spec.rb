95039a626ac84b447e9584e10a212e6fa1da8db0
Ryan Viegas <rjkviegas@gmail.com>
2020-09-10 09:07:31 +0100

    Feature Test Statement #print_to_console printd correct statement after 1 deposit and 1 withdrawal

--- spec/feature/feature_tests_spec.rb
+++ spec/feature/feature_tests_spec.rb
@@ -68,7 +68,7 @@ describe 'Feature Tests' do
         ).to_stdout
       end
 
-      it 'prints correct statement after 2 deposits t' do
+      it 'prints correct statement after 2 deposits' do
         allow(Time).to receive(:now) { time_double }
         account.deposit(500)
         account.deposit(300)
@@ -76,6 +76,17 @@ describe 'Feature Tests' do
           "date || credit || debit || balance\n"\
           "20/01/2020 || 300.00 || || 800.00\n"\
           "20/01/2020 || 500.00 || || 500.00\n"
+        ).to_stdout
+      end
+
+      it 'prints correct statement after 1 deposit and 1 withdrawal' do
+        allow(Time).to receive(:now) { time_double }
+        account.deposit(900)
+        account.withdrawal(350)
+        expect { account.statement.print_to_console }.to output(
+          "date || credit || debit || balance\n"\
+          "20/01/2020 || || 350.00 || 550.00\n"\
+          "20/01/2020 || 900.00 || || 900.00\n"
         ).to_stdout
       end
     end
